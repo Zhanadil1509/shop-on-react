0d030c3769438868554c60822730836279850c1f
Zhanadil <zhanadil1509@gmail.com>
2019-08-26 22:59:22 +0600

    src, dist, main files to work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Counter from './app'
 
 const App = () => {
   return (
     <>
-      React App!
+      <Counter />
     </>
   )
 }
