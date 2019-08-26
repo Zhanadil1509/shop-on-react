8b136a63159f2ae5bd6f49d337ef0004fe02fa0c
Zhanadil <zhanadil1509@gmail.com>
2019-08-26 21:37:34 +0600

    conf webpack, babel

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# shop-on-react
+### shop-on-react
+
+### Installing
+1. clone project
+2. npm install
+3. npm run start
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <>
+      React App!
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
