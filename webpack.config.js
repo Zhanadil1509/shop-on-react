5449834cfdf35045362eb71b1fc1c62369e3a275
Zhanadil <zhanadil1509@gmail.com>
2019-08-29 05:26:41 +0600

    conf webpack: css in js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,69 @@
-let path = require('path')
+let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let conf = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, './dist/'),
-    filename: 'main.js',
-    publicPath: 'dist/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              '@babel/plugin-transform-react-jsx',
-              '@babel/plugin-proposal-class-properties'
-            ]
-          }
-        }
-      }
-    ]
-  }
-}
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [
+                            "@babel/plugin-transform-react-jsx",
+                            "@babel/plugin-proposal-class-properties"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.module\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development'
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[local]__[sha1:hash:hex:7]'
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /^((?!\.module).)*css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        }
+                    }, 
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
 
-module.exports = conf
+module.exports = conf;
