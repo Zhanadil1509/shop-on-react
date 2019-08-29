4de6493c725d861d2ebcf3b27226ad49ab1973a1
Zhanadil <zhanadil1509@gmail.com>
2019-08-30 04:43:47 +0600

    i mobx, conf webpack set abs folder ~, create store

--- src/components/inputs/minmax/index.js
+++ src/components/inputs/minmax/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AppLazyInput from './../lazy/lazy.js';
+import AppLazyInput from '~c/inputs/lazy';
 import styles from './minmax.module.css';
 
 export default class extends React.Component{
@@ -15,6 +15,8 @@ export default class extends React.Component{
         onChange: PropTypes.func
     }
 
+    lazyInput = React.createRef();
+
     increase = () => {
         this.set(this.props.cnt + 1);
     }
@@ -26,11 +28,16 @@ export default class extends React.Component{
     set(newCnt){
         let cnt = Math.min(Math.max(newCnt, this.props.min), this.props.max);
         this.props.onChange(cnt);
+        return cnt;
     }
 
     onChange = (e) => {
         let cnt = parseInt(e.target.value);
-        this.set(isNaN(cnt) ? this.props.min : cnt);
+        let realCnt = this.set(isNaN(cnt) ? this.props.min : cnt);
+
+        if(realCnt.toString() !== e.target.value){
+            this.lazyInput.current.setValue(realCnt);
+        }
     }
 
     render(){
@@ -41,6 +48,7 @@ export default class extends React.Component{
                     nativeProps={{type: 'text', className: styles.input}}
                     value={this.props.cnt}
                     onChange={this.onChange}
+                    ref={this.lazyInput}
                 />
                 <button onClick={this.increase}>+</button>
             </div>
